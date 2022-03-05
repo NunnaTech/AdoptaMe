884916b6113b4c1e00fe4c635a7412b7cbdff2c5
AlexisLoya <alexloy117@gmail.com>
2022-03-04 20:47:28 -0600

    first commit

---
+++ src/main/java/mx/com/adoptame/AdoptaMeApplication.java
@@ -0,0 +1,13 @@
+package mx.com.adoptame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptaMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptaMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/adoptame/AdoptaMeApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.adoptame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptaMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
