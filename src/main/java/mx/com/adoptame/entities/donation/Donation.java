39372bbadd942b1aa0e5f79a8b39647131879cec
AlexisLoya <alexloy117@gmail.com>
2022-03-07 10:47:46 -0600

    Fix all typos

--- src/main/java/mx/com/adoptame/entities/donation/Donation.java
+++ src/main/java/mx/com/adoptame/entities/donation/Donation.java
@@ -1,15 +1,16 @@
-package mx.com.adoptame.entities.profile;
+package mx.com.adoptame.entities.donation;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -18,35 +19,28 @@ import org.hibernate.annotations.UpdateTimestamp;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import mx.com.adoptame.entities.address.Address;
 import mx.com.adoptame.entities.user.User;
 
 @Entity
-@Table(name = "TBL_PROFILES")
+@Table(name = "TBL_DONATIONS")
 @Data
 @NoArgsConstructor
 @ToString
-public class Profile implements Serializable{
+public class Donation implements Serializable{
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_profile")
+    @Column(name = "id_donation")
     private Integer id;
 
-    @Column(nullable = false, columnDefinition = "varchar(50)")
-    private String name;
-
-    @Column(name="last_name", nullable = false, columnDefinition = "varchar(50)")
-    private String lastName;
+    @Column(nullable = false, columnDefinition = "double(10,2)")
+    private Double quantity;
     
-    @Column(name="second_name", columnDefinition = "varchar(50)")
-    private String secondName;
+    @Column(name = "is_completed", nullable = false, columnDefinition = "TINYINT default 0")
+    private Boolean isCompleted;
 
-    @Column(columnDefinition = "varchar(17)")
-    private String phone;
-
-    @Column(columnDefinition = "varchar(150)")
-    private String image;
+    @Column(columnDefinition = "TEXT")
+    private String authorization;
 
     @CreationTimestamp
     @Column(name = "created_at", columnDefinition="TIMESTAMP")
@@ -56,15 +50,8 @@ public class Profile implements Serializable{
     @Column(name = "updated_at", columnDefinition="TIMESTAMP")
     private LocalDateTime updatedAt;
 
-    @OneToOne
-    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id", nullable=false)
     private User user;
-
-    @OneToOne
-    @JoinColumn(name = "address_id")
-    private Address address;
-
-
-    // Relationships
-
 }
+
---
+++ src/main/java/mx/com/adoptame/entities/news/News.java
@@ -0,0 +1,79 @@
+package mx.com.adoptame.entities.news;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.JoinColumn;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import mx.com.adoptame.entities.tag.Tag;
+
+@Entity
+@Table(name = "TBL_NEWS")
+public class News implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_news")
+    private Integer id;
+
+    @Column(nullable = false, columnDefinition = "varchar(50)")
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(columnDefinition = "varchar(150)")
+    private String image;
+
+    @Column(name = "is_main", nullable = false, columnDefinition = "tinyint default 0")
+    private Boolean isMain;
+
+    @Column(name = "is_published", nullable = false, columnDefinition = "tinyint default 1")
+    private Boolean isPublished;
+
+    @CreationTimestamp
+    @Column(name = "created_at", columnDefinition="TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", columnDefinition="TIMESTAMP")
+    private LocalDateTime updatedAt;
+
+
+    // Relationships
+
+    // Many to Many: TAGS
+    @ManyToMany(cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE
+    })
+    @JoinTable(name = "TBL_TAGS_NEWS",
+
+      joinColumns = @JoinColumn(name = "id_news"), 
+      inverseJoinColumns = @JoinColumn(name = "id_tag"))
+     private List<Tag> tags = new ArrayList<>();
+
+    public void addTag(Tag tag) {
+        tags.add(tag);
+        tag.getNews().add(this);
+    }
+
+    public void removeTag(Tag tag) {
+        tags.remove(tag);
+        tag.getNews().remove(this);
+    }
+}
