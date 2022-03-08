312cbbdb99aae955be0eaa857d54cc80e85d5789
AlexisLoya <alexloy117@gmail.com>
2022-03-08 13:52:32 -0600

    Fixign Color relationship

--- src/main/java/mx/com/adoptame/entities/color/Color.java
+++ src/main/java/mx/com/adoptame/entities/color/Color.java
@@ -3,13 +3,16 @@ package mx.com.adoptame.entities.color;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -23,6 +26,7 @@ import mx.com.adoptame.entities.pet.entities.Pet;
 @NoArgsConstructor
 @ToString
 public class Color implements Serializable {
+  
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,7 +39,13 @@ public class Color implements Serializable {
     @Column(nullable = false, columnDefinition = "varchar(10)")
     private String hex_code;
 
-    @ManyToMany(mappedBy = "colors")
-    public List<Pet> pets = new ArrayList<>();
 
+    @OneToMany(mappedBy="color", cascade = CascadeType.PERSIST)
+    private Set<Pet> pets;
+
+
+    public Color(String name, String hex_code) {
+        this.name = name;
+        this.hex_code = hex_code;
+    }
 }
---
+++ src/main/java/mx/com/adoptame/entities/color/ColorController.java
@@ -0,0 +1,18 @@
+package mx.com.adoptame.entities.color;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/color/")
+public class ColorController {
+    @Autowired
+    private ColorService service;
+    @GetMapping("")
+    private String hello(){
+        service.fillInicialData();
+        return "entro";
+    }
+}
