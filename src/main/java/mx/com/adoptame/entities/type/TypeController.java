4dcb438226fbc0761c81bc1b53ba6dbcca984b94
hectorsaldes <20193tn070@utez.edu.mx>
2022-03-17 14:19:17 -0600

    Adding Sweetalert for notifications and improve CRUD types

--- src/main/java/mx/com/adoptame/entities/type/TypeController.java
+++ src/main/java/mx/com/adoptame/entities/type/TypeController.java
@@ -1,4 +1,4 @@
-package mx.com.adoptame.controller;
+package mx.com.adoptame.entities.type;
 
 import lombok.extern.slf4j.Slf4j;
 import mx.com.adoptame.entities.type.Type;
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 
@@ -24,25 +25,23 @@ public class TypeController {
 
     @GetMapping("/")
     public String type(Model model) {
-        model.addAttribute("navbar", "navbar-admin");
         model.addAttribute("list", typeService.findAll());
         return "views/resources/type/typeList";
     }
 
     @GetMapping("/form")
     public String form(Model model, Type type) {
-        model.addAttribute("navbar", "navbar-admin");
         return "views/resources/type/typeForm";
     }
 
     @PostMapping("/save")
-    public String save(Model model, @Valid Type type, BindingResult bindingResult) {
+    public String save(Model model, @Valid Type type, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         try {
             if (bindingResult.hasErrors()) {
-                model.addAttribute("navbar", "navbar-admin");
                 return "views/resources/type/typeForm";
             } else {
                 typeService.save(type);
+                redirectAttributes.addFlashAttribute("noty_save",true);
             }
         } catch (Exception e) {
             log.info(e.getMessage());
@@ -53,14 +52,14 @@ public class TypeController {
     @GetMapping("/edit/{id}")
     public String edit(@PathVariable("id") Integer id, Model model, Type type){
         type = typeService.findOne(id).get();
-        model.addAttribute("navbar", "navbar-admin");
         model.addAttribute("type", type);
         return "views/resources/type/typeForm";
     }
 
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable("id") Integer id, Model model, Type type){
+    public String delete(@PathVariable("id") Integer id, Model model, Type type, RedirectAttributes redirectAttributes){
         typeService.delete(id);
+        redirectAttributes.addFlashAttribute("noty_deleted",true);
         return "redirect:/type/";
     }
 }
