44108598085eeb07801055172638c3784f652875
hectorsaldes <20193tn070@utez.edu.mx>
2022-03-18 01:50:33 -0600

    New changes like all resources cruds finished

--- src/main/java/mx/com/adoptame/config/email/EmailConfig.java
+++ src/main/java/mx/com/adoptame/config/email/EmailConfig.java
@@ -3,7 +3,6 @@ package mx.com.adoptame.config.email;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
@@ -13,15 +12,10 @@ public class EmailConfig {
     @Bean
     public JavaMailSenderImpl getJavaMailSender(){
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        // server data
-
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-
         mailSender.setUsername("adoptamenotification@gmail.com");
         mailSender.setPassword("iABu*sOT47bM4ZJd58fg");
-
-        // server and connection properties
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol","smtp");
         props.put("mail.smtp.auth","true");
