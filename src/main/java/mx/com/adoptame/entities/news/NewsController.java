8fb4ff1f45bf94505c10ecd3e0028bd8c0899032
Hector Saldaña Espinoza <20193tn070@utez.edu.mx>
2022-03-21 07:51:58 -0600

    table blogs crud added, but with a issue on edit and update

--- src/main/java/mx/com/adoptame/entities/news/NewsController.java
+++ src/main/java/mx/com/adoptame/entities/news/NewsController.java
@@ -3,7 +3,6 @@ package mx.com.adoptame.entities.news;
 import lombok.extern.slf4j.Slf4j;
 import mx.com.adoptame.entities.tag.Tag;
 import mx.com.adoptame.entities.tag.TagService;
-import mx.com.adoptame.entities.user.User;
 import mx.com.adoptame.entities.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
-import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -28,44 +26,68 @@ public class NewsController {
     @Autowired
     private UserService userService;
 
-    @GetMapping(path = {""})
+    //    Para todos
+    @GetMapping("/")
     public String home(Model model, News news) {
         model.addAttribute("navbar", "navbar-all");
-        model.addAttribute("newsList", newsService.findAll());
+        model.addAttribute("newsList", newsService.findLastFive());
         return "views/blog/blogs";
     }
 
-    @GetMapping(path = {"/admin"})
+    //    List admin
+    @GetMapping("/admin")
     public String management(Model model, News news) {
-        model.addAttribute("navbar", "navbar-admin");
-        model.addAttribute("topNews",newsService.findLastFive());
-        return "views/blog/blog-management";
+        model.addAttribute("list", newsService.findAll());
+        return "views/blog/blogList";
     }
 
-    @GetMapping(path = {"/save"})
+    //    Form admin
+    @GetMapping("/admin/form")
     public String save(Model model, News news) {
         model.addAttribute("navbar", "navbar-admin");
         model.addAttribute("tagsList", tagService.findAll());
-
-        return "views/blog/blog-create";
+        return "views/blog/blogForm";
     }
 
-    @PostMapping("/save")
-    public String save(Model model, @Valid News news, BindingResult bindingResult, RedirectAttributes redirectAttributes,
-                       @ModelAttribute("tagValues") String tagValues) {
-        news.setImage("adsadasd");
+    //    Edit admin
+    @GetMapping("/admin/edit/{id}")
+    public String edit(@PathVariable("id") Integer id, Model model, News news, RedirectAttributes redirectAttributes) {
+
+        news = newsService.findOne(id).orElse(null);
+        if (news == null) {
+            redirectAttributes.addFlashAttribute("msg_error", "Elemento no encontrado");
+            return "redirect:/blog/admin";
+        }
+        System.out.println(news.getTags().toArray().toString());
+        model.addAttribute("news", news);
+        return "views/blog/blogForm";
+
+    }
+
+    @GetMapping("/admin/delete/{id}")
+    public String delete(@PathVariable("id") Integer id, Model model, News news, RedirectAttributes redirectAttributes) {
+        if (newsService.delete(id)) {
+            redirectAttributes.addFlashAttribute("msg_success", "Blog eliminado exitosamente");
+        } else {
+            redirectAttributes.addFlashAttribute("msg_error", "Blog no eliminado");
+        }
+        return "redirect:/blog/admin";
+    }
+
+    //    Save admin
+    @PostMapping("/admin/save")
+    public String save(Model model, @Valid News news, BindingResult bindingResult, RedirectAttributes redirectAttributes, @ModelAttribute("tagValues") String tagValues) {
+        news.setImage("http://thecatandthedog.com/wp-content/uploads/2020/11/5200.jpg");
         news.setUser(userService.findOne(1).get());
         try {
             if (bindingResult.hasErrors()) {
-                model.addAttribute("navbar", "navbar-admin");
                 model.addAttribute("tagsList", tagService.findAll());
-
-                return "views/blog/blog-create";
+                return "views/blog/blogForm";
             }
             news = newsService.save(news).get();
             String[] tags = tagValues.split(",");
             System.out.println(tags);
-            for (String tag: tags) {
+            for (String tag : tags) {
                 Optional<Tag> tagItem = tagService.findOne(Integer.valueOf(tag));
                 if (tagItem.isPresent()) {
                     newsService.saveTag(news, tagItem.get());
