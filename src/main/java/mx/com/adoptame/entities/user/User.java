831582faa7435df6310bb81d40cd9fadf2d59bde
hectorsaldes <20193tn070@utez.edu.mx>
2022-03-21 18:23:59 -0600

    modified some changed like reason on request

--- src/main/java/mx/com/adoptame/entities/user/User.java
+++ src/main/java/mx/com/adoptame/entities/user/User.java
@@ -14,6 +14,7 @@ import javax.validation.constraints.Size;
 
 import mx.com.adoptame.entities.boards.Board;
 import mx.com.adoptame.entities.log.Log;
+import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
