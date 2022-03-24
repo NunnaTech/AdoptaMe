043debe29381944d021b6631bb1e495f5e18e2ff
hectorsaldes <20193tn070@utez.edu.mx>
2022-03-23 23:47:26 -0600

    fixing changes of all branches

--- src/main/java/mx/com/adoptame/config/email/EmailConfig.java
+++ src/main/java/mx/com/adoptame/config/email/EmailConfig.java
@@ -2,7 +2,6 @@ package mx.com.adoptame.config.email;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
