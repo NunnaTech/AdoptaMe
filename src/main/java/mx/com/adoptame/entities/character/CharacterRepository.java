08863aa7047404e27df6ae405cf2dcacc2608ead
AlexisLoya <alexloy117@gmail.com>
2022-03-26 20:15:00 -0600

    Adding initial data, fix changes inthe databases and clear some code

--- src/main/java/mx/com/adoptame/entities/character/CharacterRepository.java
+++ src/main/java/mx/com/adoptame/entities/character/CharacterRepository.java
@@ -3,7 +3,10 @@ package mx.com.adoptame.entities.character;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CharacterRepository extends CrudRepository<Character,Integer> {
+    List<Character> findAllByStatus(Boolean status);
     
 }
