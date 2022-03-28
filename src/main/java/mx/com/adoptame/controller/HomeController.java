464aaccc80aae87e1829bfc0ff84208329f03647
AlexisLoya <alexloy117@gmail.com>
2022-03-28 00:07:07 -0600

    adding some necesary method in: pets, news, requets

--- src/main/java/mx/com/adoptame/controller/HomeController.java
+++ src/main/java/mx/com/adoptame/controller/HomeController.java
@@ -1,16 +1,21 @@
 package mx.com.adoptame.controller;
 
+import mx.com.adoptame.entities.news.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller()
 public class HomeController {
-
+    @Autowired
+    private NewsService newsService;
     @GetMapping(path = {"/"})
     public String index(Model model) {
         model.addAttribute("navbar", "navbar-all");
         model.addAttribute("fix", "fix");
+        model.addAttribute("mainNews", newsService.findMainNews());
+
         return "index";
     }
 
--- src/main/java/mx/com/adoptame/entities/news/NewsRepository.java
+++ src/main/java/mx/com/adoptame/entities/news/NewsRepository.java
@@ -15,7 +15,7 @@ public interface NewsRepository extends CrudRepository<News, Integer> {
 
     List<News> findAllByIsPublished(Boolean isPublished);
 
-    List<News> findAllByIsMainAndIsPublished(Boolean isMain, Boolean isPublished);
+    List<News> findAllByIsMainAndIsPublishedOrderByCreatedAtDesc(Boolean isMain, Boolean isPublished);
 
 
 }
