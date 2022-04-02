e37be607dfb32f97d7fd123bea47455d9bbdfff7
hectorsaldes <20193tn070@utez.edu.mx>
2022-04-02 16:50:50 -0600

    Donations by user almost finished

--- src/main/java/mx/com/adoptame/entities/donation/DonationController.java
+++ src/main/java/mx/com/adoptame/entities/donation/DonationController.java
@@ -1,10 +1,14 @@
 package mx.com.adoptame.entities.donation;
 
+import mx.com.adoptame.entities.user.User;
+import mx.com.adoptame.entities.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/donation")
@@ -13,9 +17,20 @@ public class DonationController {
     @Autowired
     private DonationService donationService;
 
-    @GetMapping("/")
-    public String type(Model model) {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/admin")
+    public String donation(Model model) {
         model.addAttribute("list", donationService.findAll());
+        return "views/donations";
+    }
+
+    @GetMapping("/")
+    public String owns(Model model) {
+        // TODO obtener el id de la sesión para obtener las donaciones de la persona
+        Optional<User> user = userService.findOne(2);
+        model.addAttribute("list", user.get().getDonations());
         return "views/donations";
     }
 
