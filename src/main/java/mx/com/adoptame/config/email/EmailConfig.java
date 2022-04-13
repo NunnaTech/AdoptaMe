897ffe6e0f7ddaf0e03eccf4507eff388429f5e7
AlexisLoya <alexloy117@gmail.com>
2022-04-13 13:04:27 -0500

    hotfix: email

--- src/main/java/mx/com/adoptame/config/email/EmailConfig.java
+++ src/main/java/mx/com/adoptame/config/email/EmailConfig.java
@@ -1,5 +1,6 @@
 package mx.com.adoptame.config.email;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -8,18 +9,22 @@ import java.util.Properties;
 
 @Configuration
 public class EmailConfig {
+    @Value("${email-address}")
+    private String emailAddress;
+    @Value("${email-password}")
+    private String emailPassword;
+
     @Bean
     public JavaMailSenderImpl getJavaMailSender(){
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("adoptamenotification@gmail.com");
-        mailSender.setPassword("iABu*sOT47bM4ZJd58fg");
+        mailSender.setUsername(emailAddress);
+        mailSender.setPassword(emailPassword);
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol","smtp");
         props.put("mail.smtp.auth","true");
         props.put("mail.smtp.starttls.enable","true");
-        //props.put("mail.debug", "true");
         return mailSender;
     }
 }
